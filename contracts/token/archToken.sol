1fc515accb84d43933cf48da5c47abc60228b60a
webster <websterchan1998@gmail.com>
2023-05-18 12:59:48 +0200

    Fixed bugs in requestPayment

--- contracts/token/archToken.sol
+++ contracts/token/archToken.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+
+
+
 contract ARCHT is ERC20, AccessControl {
 
     // =========================================================================
@@ -12,8 +15,8 @@ contract ARCHT is ERC20, AccessControl {
     // =========================================================================
 
     uint256 public nextIncreaseTimestamp;
-    mapping (address => uint256) public pendingWithdrawals;
-    mapping (address => mapping (address => uint256)) public deposits;
+    mapping(address => uint256) public pendingWithdrawals;
+    mapping(address => mapping(address => uint256)) public deposits;
     uint256 public monthlyTransactionCount;
     uint256 public lastMonthTransactionCount;
 
@@ -21,6 +24,7 @@ contract ARCHT is ERC20, AccessControl {
     // =========================================================================
     //                               Roles
     // =========================================================================
+    
     bytes32 public ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     // =========================================================================
@@ -32,31 +36,30 @@ contract ARCHT is ERC20, AccessControl {
         nextIncreaseTimestamp = block.timestamp + 30 days;
     }
 
-
     // =========================================================================
     //                               Functions
     // =========================================================================
     function increaseSupply() external onlyRole(ADMIN_ROLE) {
         require(block.timestamp >= nextIncreaseTimestamp, "Increase not allowed yet");
-        
+        // Monthly Supply Increase Amount <= 100 * (Monthly transaction this month - Monthly transaction from last month)
         if (monthlyTransactionCount > lastMonthTransactionCount) {
             uint256 increaseFactor = monthlyTransactionCount - lastMonthTransactionCount;
-            uint256 increase_amount = increaseFactor * 100 * (10 ** uint256(decimals())); 
-            if (increase_amount <= totalSupply() * 3 / 2) {
-                increase_amount = totalSupply() * 3 / 2;
+            uint256 increaseAmount = increaseFactor * 100 * (10 ** decimals());
+            if (increaseAmount <= totalSupply() * 3 / 2) {
+                increaseAmount = totalSupply() * 3 / 2;
             }
-            _mint(msg.sender, increase_amount);
+            _mint(msg.sender, increaseAmount);
         }
         lastMonthTransactionCount = monthlyTransactionCount;
         monthlyTransactionCount = 0;
-        nextIncreaseTimestamp = block.timestamp + 30 days;    
-        }
+        nextIncreaseTimestamp = block.timestamp + 30 days; //Supply can only be increased every 30 days
+    }
 
     function batchTransfer(address[] memory recipients, uint256[] memory amounts) external {
         require(recipients.length == amounts.length, "Mismatched inputs");
 
         for (uint256 i = 0; i < recipients.length; i++) {
-            transferFrom(msg.sender, recipients[i], amounts[i]);
+            transfer(recipients[i], amounts[i]);
         }
     }
 
@@ -76,13 +79,23 @@ contract ARCHT is ERC20, AccessControl {
     function confirmPayment(address payee) external {
         uint256 amount = pendingWithdrawals[payee];
         require(amount > 0, "No tokens to confirm");
-
+        transfer(payee, amount);
         pendingWithdrawals[payee] = 0;
-        transferFrom(address(this), payee, amount);
         monthlyTransactionCount++;
     }
-    
+
     function burn(address from, uint256 amount) external onlyRole(ADMIN_ROLE) {
         _burn(from, amount);
     }
+
+    // Additional Functions
+    function approveDepositSpender(address spender, uint256 amount) external {
+        address depositor = msg.sender;
+        _approve(depositor, spender, amount);
+    }
+
+    function getDepositAllowance(address depositor, address spender) external view returns (uint256) {
+        return allowance(depositor, spender);
+    }
+
 }
