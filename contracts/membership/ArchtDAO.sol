a197a0d5875f7325e9ce990804ac2c7f171b4baa
webster <websterchan1998@gmail.com>
2023-05-22 17:05:19 +0200

    updated implementation

--- contracts/membership/ArchtDAO.sol
+++ contracts/membership/ArchtDAO.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract ArchtDAO is ERC721, ERC721URIStorage, ERC721Burnable, AccessControl, EIP712, ERC721Votes {
+contract ArchtDAO is ERC721, ERC721URIStorage, AccessControl, EIP712, ERC721Votes {
     using Counters for Counters.Counter;
     
     // =========================================================================
@@ -98,10 +97,15 @@ contract ArchtDAO is ERC721, ERC721URIStorage, ERC721Burnable, AccessControl, EI
     }
 
 
-    //Burn a token when the membership is removed
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
+    // Override the burn function to allow admin to burn any token
+    function burn(uint256 tokenId) public virtual onlyRole(ADMIN_ROLE) {
+        _burn(tokenId);
         _totalSupply.decrement();
+    }
+
+    // Ensure correct execution of _burn in inherited classes
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) onlyRole(ADMIN_ROLE){
+        super._burn(tokenId);
     }
 
     // Return true if that address has membership of the DAO
